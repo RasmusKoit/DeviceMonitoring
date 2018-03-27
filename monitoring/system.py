c1bc3d437ade8eb7d5974a2a5a0dd3a0efc52aa6
RasmusKoit <rasmuskoit@gmail.com>
2018-03-27 17:44:03 +0300

    Finished functions for getting info from CPU, Mem and Disk

---
+++ monitoring/system.py
@@ -0,0 +1,27 @@
+"""Simple system monitor"""
+import psutil
+
+
+def get_cpu_usage(interval = 1.0, per_cpu = False):
+    return psutil.cpu_percent(interval=interval, percpu=per_cpu)
+
+
+def get_mem_usage(option = "all"):
+    mem = psutil.virtual_memory()
+    if option == "all":
+        return mem
+    elif option == "total":
+        return mem.total
+    elif option == "available":
+        return mem.available
+    elif option == "percent":
+        return mem.percent
+    elif option == "used":
+        return mem.used
+    elif option == "free":
+        return mem.free
+
+
+if __name__ == '__main__':
+    print(get_cpu_usage())
+    print(get_mem_usage().percent)
