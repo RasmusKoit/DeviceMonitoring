af924f386fd63457df926cb780782d9449ab285a
Rasmus Koit <rasmuskoit@gmail.com>
2018-03-29 14:05:00 +0300

    Finished functions for getting info from CPU, Mem and Disk

--- sendData/sendData.py
+++ sendData/sendData.py
@@ -2,34 +2,36 @@ import requests
 from monitoring import system
 import json
 
-pc = system
-data = {}
-## CPU, Memory, Disk
-data['CPU'] = {}
-data['CPU']['percent'] = pc.get_cpu_usage()
 
-data['Memory'] = {}
-data['Memory']['all'] = pc.get_mem_usage("all")
-data['Memory']['total'] = pc.get_mem_usage("total")
-data['Memory']['available'] = pc.get_mem_usage("available")
-data['Memory']['percent'] = pc.get_mem_usage("percent")
-data['Memory']['used'] = pc.get_mem_usage("used")
-data['Memory']['free'] = pc.get_mem_usage("free")
+def collectData():
 
-data['Disk'] = {}
-data['Disk']['all'] = pc.get_disk_usage("all")
-data['Disk']['total'] = pc.get_disk_usage("total")
-data['Disk']['percent'] = pc.get_disk_usage("percent")
-data['Disk']['used'] = pc.get_disk_usage("used")
-data['Disk']['free'] = pc.get_disk_usage("free")
+    pc = system
+    data = {}
+    ## CPU, Memory, Disk
+    data['CPU'] = {}
+    data['CPU']['percent'] = pc.get_cpu_usage()
 
-json_data = json.dumps(data)
+    data['Memory'] = {}
+    data['Memory']['all'] = pc.get_mem_usage("all")
+    data['Memory']['total'] = pc.get_mem_usage("total")
+    data['Memory']['available'] = pc.get_mem_usage("available")
+    data['Memory']['percent'] = pc.get_mem_usage("percent")
+    data['Memory']['used'] = pc.get_mem_usage("used")
+    data['Memory']['free'] = pc.get_mem_usage("free")
 
-def pp_json(json_thing, sort=True, indents=4):
-    if type(json_thing) is str:
-        print(json.dumps(json.loads(json_thing), sort_keys=sort, indent=indents))
-    else:
-        print(json.dumps(json_thing, sort_keys=sort, indent=indents))
-    return None
+    data['Disk'] = {}
+    data['Disk']['all'] = pc.get_disk_usage("all")
+    data['Disk']['total'] = pc.get_disk_usage("total")
+    data['Disk']['percent'] = pc.get_disk_usage("percent")
+    data['Disk']['used'] = pc.get_disk_usage("used")
+    data['Disk']['free'] = pc.get_disk_usage("free")
 
-pp_json(json_data)
+    return json.dumps(data)
+
+def sendRequest(url, data):
+
+    url = 'https://api.github.com/some/endpoint'
+    payload = {'some': 'data'}
+    headers = {'content-type': 'application/json'}
+
+    r = requests.post(url, data=json.dumps(payload), headers=headers)
